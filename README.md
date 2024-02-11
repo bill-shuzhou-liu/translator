4678b738b5aa18695b53abcd311b4c1c1e80167d
Bill Liu <bliu@gmail.com>
2024-02-11 19:43:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+The python code to auto detect and transalte text to English
+
+# install and run
+```
+pip install -e .
+python3 -m pytranslator
+```
---
+++ src/pytranslator/__main__.py
@@ -0,0 +1,5 @@
+import translators as ts
+
+if __name__ == '__main__':
+    res = ts.translate_text("Còn đợi Merge với confirm nữa là xong", to_language='en')
+    print(res)
